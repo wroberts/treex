7a420239b9564955fa8030a928bee2858db5ac72
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-10-08 17:54:24 +0200

    changes to EN2ES scenarios for HU EN->ES baseline expt

--- lib/Treex/Scen/Transfer/EN2ES.pm
+++ lib/Treex/Scen/Transfer/EN2ES.pm
@@ -59,6 +59,7 @@ sub get_scenario_string {
     my $TM_DIR= 'data/models/translation/en2es';
     my $IT_LEMMA_MODELS = '';
     my $IT_FORMEME_MODELS = '';
+    $self->{tm_adaptation} = 'no';
     if ($self->tm_adaptation eq 'interpol'){
         $IT_LEMMA_MODELS = "static 0.5 IT/20150728_batch1a-tlemma.static.gz\n      maxent 1.0 IT/20150728_batch1a-tlemma.maxent.gz";
         $IT_FORMEME_MODELS = "static 1.0 IT/20150728_batch1a-formeme.static.gz\n      maxent 0.5 IT/20150728_batch1a-formeme.maxent.gz";
@@ -70,12 +71,12 @@ sub get_scenario_string {
     $self->gazetteer ? 'T2T::TrGazeteerItems src_lang='.$self->src_lang : (),
     'T2T::EN2ES::TrLTryRules',
     "T2T::TrFAddVariantsInterpol model_dir=$TM_DIR models='
-      static 1.0 Pilot1_formeme.static.gz
-      maxent 0.5 Pilot1_formeme.maxent.gz
+      static 1.0 hu_trained_en2es_formeme.static.gz
+      maxent 0.5 hu_trained_en2es_formeme.maxent.gz
       $IT_FORMEME_MODELS'",
     "T2T::TrLAddVariantsInterpol model_dir=$TM_DIR models='
-      static 0.5 Pilot1_tlemma.static.gz
-      maxent 1.0 Pilot1_tlemma.maxent.gz
+      static 0.5 hu_trained_en2es_tlemma.static.gz
+      maxent 1.0 hu_trained_en2es_tlemma.maxent.gz
       $IT_LEMMA_MODELS'",
     $self->fl_agreement ? 'T2T::FormemeTLemmaAgreement fun='.$self->fl_agreement : (),
     'Util::DefinedAttr tnode=t_lemma,formeme message="after simple transfer"',
