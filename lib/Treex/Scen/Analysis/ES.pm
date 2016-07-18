a848a694a9c92e53beb35df37783ace5fd283615
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-07-18 10:16:53 +0200

    esen-indomain-baseline

--- lib/Treex/Scen/Analysis/ES.pm
+++ lib/Treex/Scen/Analysis/ES.pm
@@ -57,7 +57,6 @@ sub get_scenario_string {
     'A2T::MarkReflpronCoref',
     'A2T::ES::FixReflexiveVerbs',
 #    'A2T::ES::FixTlemmas',
-+    'MarkMWEs phrase_list_path=/home/anglistik/robertsw/.treex/share/data/models/translation/en2eu/esen-indomain-theta0.3-v1_mwes_trained.utf8.tsv',
     ;
 
     return $scen;
