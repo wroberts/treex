1c933634e27a854d979d476214481075f8f46554
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-07-18 10:24:01 +0200

    esen-europarl-theta0.1-v1 fixed MarkMWEs

--- lib/Treex/Scen/Analysis/ES.pm
+++ lib/Treex/Scen/Analysis/ES.pm
@@ -57,6 +57,7 @@ sub get_scenario_string {
     'A2T::MarkReflpronCoref',
     'A2T::ES::FixReflexiveVerbs',
 #    'A2T::ES::FixTlemmas',
+    'MarkMWEs phrase_list_path=/home/anglistik/robertsw/.treex/share/data/models/translation/en2eu/esen-europarl-theta0.1-v1_mwes_trained.utf8.tsv',
     ;
 
     return $scen;
