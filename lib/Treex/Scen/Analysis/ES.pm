1756cfce6affc24431649c00cc6980ea4d99afdd
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-09-07 01:17:18 +0200

    esen-indomain-theta0.1

--- lib/Treex/Scen/Analysis/ES.pm
+++ lib/Treex/Scen/Analysis/ES.pm
@@ -57,7 +57,7 @@ sub get_scenario_string {
     'A2T::MarkReflpronCoref',
     'A2T::ES::FixReflexiveVerbs',
 #    'A2T::ES::FixTlemmas',
-    'MarkMWEs phrase_list_path=/home/anglistik/robertsw/.treex/share/data/models/translation/en2eu/esen-indomain-theta0.2_mwes_trained.utf8.tsv',
+    'MarkMWEs phrase_list_path=/home/anglistik/robertsw/.treex/share/data/models/translation/en2eu/esen-indomain-theta0.1_mwes_trained.utf8.tsv',
     ;
 
     return $scen;
